158175562fb1fd44644126d2bec683a3fad0bcba
Mirakuru Developer <student@mirakuru.local>
2026-08-24 20:53:20 -0500

    feature: init proyect

---
+++ nexusmarket/nexusmarket/src/main/java/application/NexusmarketApplication.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusmarketApplication.class, args);
+	}
+
+}
